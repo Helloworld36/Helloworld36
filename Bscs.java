4316624d4dd70f452e8305156ca7af3aa84f53ca
Helloworld36 <105580634+Helloworld36@users.noreply.github.com>
2022-07-22 17:14:59 +0500

    Add files via upload

---
+++ Bscs.java
@@ -0,0 +1,7 @@
+class Bscs2k21G
+{
+public static void main(String arg[])
+{
+ System.out.println("hello world");
+}
+}
---
+++ MainDemoStringCmd.java
@@ -0,0 +1,18 @@
+class MainDemoStringCMD
+{
+public static void main(String[]args)
+{
+int[] m=new int[5];
+int b=10;
+for(int i=0;i<5;i++)
+
+{
+b=b+10;
+m[i]=b;
+}
+for(int i=0;i<5;i++)
+{
+System.out.println(m[i]);
+}
+}
+}
